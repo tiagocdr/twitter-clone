9c1f929e68b291da34e959f6d091908efcca0129
santi <tiagoelcoder@gmail.com>
2021-03-02 22:14:03 -0500

    SignUp Working

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from authentication.form import LoginForm, SignUpForm
 from django.contrib.auth import authenticate, login
+from twitteruser.models import TwitterUser
 # Create your views here.
 
 def login_view(request):
@@ -19,5 +20,17 @@ def login_view(request):
     return render(request, 'form.html', {'form': form})
 
 def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = TwitterUser.objects.create(
+                username=data['username'], password=data['password']
+                )
+            login(request, user)
+            return HttpResponseRedirect(
+                request.GET.get('next', reverse('home'))
+                )
+            
     form = SignUpForm()
     return render(request, 'form.html', {'form': form})
